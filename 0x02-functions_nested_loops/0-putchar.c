22508695a2bd15d803e43e7543badd76adb707eb
{mecus4life} <{mecus4life@gmail.com}>
2022-09-13 14:48:27 -0700

    First 8 script

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - debugging example
- * Return: 0
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char *_putchar = "_putchar";
-
-	while (*_putchar)
-	{
-		_putchar(*_putchar);
-		_putchar++;
-	}
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
