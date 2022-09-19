ceceea81a44d45084fe842c28de4d5d64a7ec65d
{mecus4life} <{mecus4life@gmail.com}>
2022-09-19 06:50:07 -0700

    arrarys

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,41 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies a string pointed to by @src, including the
- *           terminating null byte, to a buffer pointed to by @dest.
- * @dest: A buffer to copy the string to.
- * @src: The source string to copy.
+ * *_strcpy - copies the string pointed to by src.
  *
- * Return: A pointer to the destination string @dest.
+ * @dest: pointer to the buffer.
+ * @src: pointer to the source string.
+ * Return: char
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int n;
+	int i;
 
-	while (src[index])
+	n = string_length(src);
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
 	}
-
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
