5aaf080b5a1d239269476dea33fee4407885585d
{mecus4life} <{mecus4life@gmail.com}>
2022-09-19 06:59:28 -0700

    arrarys

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,7 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	n = string_length(src);
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++);
 	{
 		dest[i] = src[i];
 	}
@@ -33,7 +33,7 @@ int string_length(char *pointer)
 {
 	int c = 0;
 
-	while (*(pointer + c) != '\0')
+	while (*(pointer + c) != '\0');
 	{
 		c++;
 	}
