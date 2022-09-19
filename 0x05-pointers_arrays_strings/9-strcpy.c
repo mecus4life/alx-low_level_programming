0720592b260adec7c9e62395cc497ac5f1a80910
{mecus4life} <{mecus4life@gmail.com}>
2022-09-19 06:07:18 -0700

    all

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -23,19 +23,3 @@ char *_strcpy(char *dest, char *src)
 	}
 	return (dest);
 }
-
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
