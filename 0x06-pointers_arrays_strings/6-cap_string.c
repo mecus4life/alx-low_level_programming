d3ef0fb6b7af15e373fd2a4dd93105d147222b78
{mecus4life} <{mecus4life@gmail.com}>
2022-09-21 13:51:19 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,33 @@
 #include "main.h"
 
 /**
- * *cap_string - capitalizes all words of a string.
- *
- * @str: hold character.
- * Return: string.
+ * cap_string - capitalizes all word of a string
+ * @str: the string
+ * Return: str with it's words capitalized
  */
+
 char *cap_string(char *str)
 {
-	int i;
+	int i, j;
+	char s[] =  {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; str[i] != '\0'; i++)
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	i = 1;
+	while (str[i] != '\0')
 	{
-		if (i == 0)
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if ((str[i] >= 97 && str[i] <= 122))
+			for (j = 0; j < 13; j++)
 			{
-				str[i] = str[i] - 32;
-				continue;
+				if (str[i - 1] == s[j])
+				{
+					str[i] -= 32;
+				}
 			}
 		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
+		i++;
 	}
+
 	return (str);
 }
