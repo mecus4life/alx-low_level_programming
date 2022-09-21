390b03117f705878921755a217e3c48bc1c351da
{mecus4life} <{mecus4life@gmail.com}>
2022-09-21 13:26:01 -0700

    function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,58 @@
 #include "main.h"
 
 /**
- * _strncpy - a function that copys the strings.
- * @dest: an input string
- * @src: an input string
- * @n: an input integer
- * Return: A pointer to the resulting string
+ * _strlen - function that return the lenght of a string
+ * @s: the string
+ * Return: length of s
  */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i))
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strncpy - function that copies the string pointed by src, included the
+ * teminate null byte (\0), to the buffer pointed to by dest
+ * @dest: the first parameter
+ * @src: second prameter
+ * @n: bytes from src that the function will use
+ * Return: the pointer to dest
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int ld, ls, nn;
 
-	while (*src)
+	ld = _strlen(dest);
+	ls = _strlen(src) - 1;
+
+	if (n <= 0)
+		return (dest);
+	n --;
+
+	if (n > ls)
+		n = ls;
+	while (n >= 0)
 	{
-		srclen++;
-		src++;
+		*(dest + n) = *(src + n);
+		n--;
 	}
 
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+	nn = ld - n;
+	if (nn > 0)
+	{
+		while (nn > 0)
+		{
+			*(dest + ls + nn) = '\0';
+			nn--;
+		}
+	}
+	return (dest);
 }
