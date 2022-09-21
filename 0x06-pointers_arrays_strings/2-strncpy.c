8ba14339615a0d15fdefb9ee3946830c46da51fb
{mecus4life} <{mecus4life@gmail.com}>
2022-09-21 13:28:20 -0700

    function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,58 +1,22 @@
 #include "main.h"
 
 /**
- * _strlen - function that return the lenght of a string
- * @s: the string
- * Return: length of s
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: If c is found - a pointer to the first occurence.
+ *         If c is not found - NULL.
  */
-
-int _strlen(char *s)
+char *_strchr(char *s, char c)
 {
-	int i;
+	int index;
 
-	i = 0;
-	while (*(s + i))
-		i++;
-
-	return (i);
-}
-
-/**
- * _strncpy - function that copies the string pointed by src, included the
- * teminate null byte (\0), to the buffer pointed to by dest
- * @dest: the first parameter
- * @src: second prameter
- * @n: bytes from src that the function will use
- * Return: the pointer to dest
- */
-
-char *_strncpy(char *dest, char *src, int n)
-{
-	int ld, ls, nn;
-
-	ld = _strlen(dest);
-	ls = _strlen(src) - 1;
-
-	if (n <= 0)
-		return (dest);
-	n --;
-
-	if (n > ls)
-		n = ls;
-	while (n >= 0)
+	for (index = 0; s[index] >= '\0'; index++)
 	{
-		*(dest + n) = *(src + n);
-		n--;
+		if (s[index] == c)
+			return (s + index);
 	}
 
-	nn = ld - n;
-	if (nn > 0)
-	{
-		while (nn > 0)
-		{
-			*(dest + ls + nn) = '\0';
-			nn--;
-		}
-	}
-	return (dest);
+	return ('\0');
 }
