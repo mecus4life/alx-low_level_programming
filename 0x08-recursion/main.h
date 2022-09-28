88049870ee4d77fdfa7f22b2d2fd74d415d2f943
{mecus4life} <{mecus4life@gmail.com}>
2022-09-28 02:37:03 -0700

    print strings

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int);
+
+#endif
