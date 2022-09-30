83a031074da97b899d5930877795433e3a515cf3
{mecus4life} <{mecus4life@gmail.com}>
2022-09-30 07:59:26 -0700

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string
- * @str: string input
- * Return: string
-*/
-
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
+ */
 void _puts(char *str)
 {
-	while (*str)
-	{
-		_putchar(*str++);
-	}
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
