dd2d3e7586820b2959236e51481a3fa03e1d4ee6
mecus4life <{mecus4life@gmail.com}>
2022-10-24 20:45:33 -0700

    list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,47 +1,40 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * of a linked list.
- * @head: head of a list.
- * @index: index of the list where the node is
- * deleted.
+ * delete_nodeint_at_index - deletes the node at index index of a linked list.
  *
- * Return: 1 if it succeeded, -1 if it failed.
+ * @head: pointer to pointer to head.
+ * @index: position of the node to be deleted.
+ * Return: return 1 if success, -1 if failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *prev;
-	listint_t *next;
+	listint_t *temp, *next;
 
-	prev = *head;
-
-	if (index != 0)
-	{
-		for (i = 0; i < index - 1 && prev != NULL; i++)
-		{
-			prev = prev->next;
-		}
-	}
-
-	if (prev == NULL || (prev->next == NULL && index != 0))
-	{
+	/* if linked list is empty */
+	if (*head == NULL)
 		return (-1);
-	}
 
-	next = prev->next;
-
-	if (index != 0)
+	temp = *head;
+	/* if head needs to be removed */
+	if (index == 0)
 	{
-		prev->next = next->next;
-		free(next);
+		*head = temp->next; /* change head */
+		free(temp); /* free old head */
+		return (1);
 	}
-	else
+	for (i = 0; temp != NULL && i < index - 1; i++)
 	{
-		free(prev);
-		*head = next;
+		temp = temp->next;
 	}
+	/* if index is greater than the number of nodes */
+	if (temp == NULL || temp->next == NULL)
+		return (-1);
+	next = temp->next->next;
 
+	free(temp->next);
+	temp->next = next;
 	return (1);
 }
